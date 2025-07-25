2c7535fb07dc49ffd3ad6af21a6ef9d36a615355
vishesh2539 <123531844+vishesh2539@users.noreply.github.com>
2025-07-25 21:51:00 +0530

    Update first.java

--- level-1-Program/first.java
+++ level-1-Program/first.java
@@ -1,1 +1,6 @@
-
+// Q1. Write a program that prints "Welcome to Bridgelabz!" to the screen.
+public class main{
+  public static void main(String[] args){
+    System.out.println("Welcome to BridgeLab ");
+  }
+}
